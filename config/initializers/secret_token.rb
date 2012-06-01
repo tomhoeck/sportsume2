8f46a96587bf7429be72841b8b102a96b7fba94a
Tom Hoeck <tomhoeck@gmail.com>
2012-05-31 17:37:48 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sportsume2::Application.config.secret_token = '090aea6fd2a984e80424bcc18591b130f93c4b566480a8c31383125ad2bcb5c4a8cb2c5616e969b921ca4b55fae2ce273560dd46cdf81bde5fc24a4313a85cdb'
